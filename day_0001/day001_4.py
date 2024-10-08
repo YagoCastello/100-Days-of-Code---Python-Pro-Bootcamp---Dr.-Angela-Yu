120fac0af45cf415ea8dd14eb9681a48f5f14de1
Yago Castello <104936515+YagoCastello@users.noreply.github.com>
2024-10-08 10:44:16 -0300

    Day001

---
+++ day_0001/day001_4.py
@@ -0,0 +1,5 @@
+nome = 'Jack'
+print(nome)
+
+print('Your name have ',(len(input('What is your name?'))),' size')
+print('Your your favorite food have ',(len(input('What is your favorite food ? '))))
---
+++ day_0001/day001_5.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator.')
+city = input('Which city didi you grow up in? ')
+pet = input('What is the name of a pet? ')
+print('Your band name could be: '+city+' '+pet)
